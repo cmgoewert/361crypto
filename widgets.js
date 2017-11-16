e9e63fe41be1f7fd1fdbdfc3764ce96f5ee397ea
Justin <jrosenthal96@gmail.com>
2017-11-16 12:29:25 -0500

    Adding Luke's JS

---
+++ widgets.js
@@ -0,0 +1,46 @@
+(function (b, i, t, C, O, I, N) {
+  window.addEventListener('load', function () {
+    if (b.getElementById(C)) return;
+    I = b.createElement(i), N = b.getElementsByTagName(i)[0];
+    I.src = t;
+    I.id = C;
+    N.parentNode.insertBefore(I, N);
+  }, false)
+})(document, 'script', 'https://widgets.bitcoin.com/widget.js', 'btcwdgt');
+
+rssfeed_url = new Array();
+rssfeed_url[0] = "https:news.google.com/news/rss/search/section/q/cryptocurrency/cryptocurrency?hl=en&gl=US&ned=us";
+rssfeed_frame_width = "230";
+rssfeed_frame_height = "400";
+rssfeed_scroll = "on";
+rssfeed_scroll_step = "6";
+rssfeed_scroll_bar = "off";
+rssfeed_target = "_blank";
+rssfeed_font_size = "12";
+rssfeed_font_face = "";
+rssfeed_border = "on";
+rssfeed_css_url = "";
+rssfeed_title = "on";
+rssfeed_title_name = "Cryptocurrency Headlines";
+rssfeed_title_bgcolor = "#ffcc00";
+rssfeed_title_color = "#fff";
+rssfeed_title_bgimage = "https:themerkle.com/wp-content/uploads/2017/02/rare-cryptocurrency.jpg";
+rssfeed_footer = "off";
+rssfeed_footer_name = "";
+rssfeed_footer_bgcolor = "#fff";
+rssfeed_footer_color = "#333";
+rssfeed_footer_bgimage = "";
+rssfeed_item_title_length = "50";
+rssfeed_item_title_color = "#000";
+rssfeed_item_bgcolor = "#e6f9ff";
+rssfeed_item_bgimage = "";
+rssfeed_item_border_bottom = "off";
+rssfeed_item_source_icon = "off";
+rssfeed_item_date = "on";
+rssfeed_item_description = "on";
+rssfeed_item_description_length = "300";
+rssfeed_item_description_color = "#666";
+rssfeed_item_description_link_color = "#333";
+rssfeed_item_description_tag = "off";
+rssfeed_no_items = "0";
+rssfeed_cache = "7b756af95616fab89d00b65e0fc4eff5";
