4682948d80d6a269019c8e1bf9b20b24bd585b62
Justin <jrosenthal96@gmail.com>
2017-12-05 22:14:19 -0500

    Remove counter

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,17 +1,13 @@
-let counter = 0;
-
-function newTransaction(transaction) {
+function newTransaction(index, transaction) {
   let btcusd = parseInt($(".btcusd").html().replace(',', ''));
 
-  $("table.transactions tbody").append("<tr><td>" + counter + "</td><td>BTC</td><td>" + transaction.amount + "</td><td>$" + transaction.buyPrice + "</td><td>$" + (transaction.amount * btcusd) + "</td></tr>");
-  counter++;
+  $("table.transactions tbody").append("<tr><td>" + index + "</td><td>BTC</td><td>" + transaction.amount + "</td><td>$" + transaction.buyPrice + "</td><td>$" + (transaction.amount * btcusd) + "</td></tr>");
 }
 
 $(function () {
   getTransactions(Cookies.get('username'), function (data) {
     data.forEach(function (val, index) {
-      counter = index;
-      newTransaction(val);
+      newTransaction(index, val);
     });
   });
 });
