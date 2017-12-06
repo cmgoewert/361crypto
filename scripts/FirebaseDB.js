57eb2f3ac90e5986b02cbde31232c866d9284bbc
Justin <jrosenthal96@gmail.com>
2017-12-05 23:23:10 -0500

    Add transactions

--- scripts/FirebaseDB.js
+++ scripts/FirebaseDB.js
@@ -1,30 +1,28 @@
-
 //initilialize firebase
 var config = {
-    apiKey: "AIzaSyDWJkjY3_1T-x4gPnRQem5n80w-9gv_Oec",
-    authDomain: "cryptotracker-bb2f2.firebaseapp.com",
-    databaseURL: "https://cryptotracker-bb2f2.firebaseio.com",
-    projectId: "cryptotracker-bb2f2",
-    storageBucket: "cryptotracker-bb2f2.appspot.com",
-    messagingSenderId: "839599671002"
+  apiKey: "AIzaSyDWJkjY3_1T-x4gPnRQem5n80w-9gv_Oec",
+  authDomain: "cryptotracker-bb2f2.firebaseapp.com",
+  databaseURL: "https://cryptotracker-bb2f2.firebaseio.com",
+  projectId: "cryptotracker-bb2f2",
+  storageBucket: "cryptotracker-bb2f2.appspot.com",
+  messagingSenderId: "839599671002"
 };
 
 firebase.initializeApp(config);
 var database = firebase.database();
 
 function getCurrUser(username, callback) {
-    database.ref("Users/" + username).once("value").then(snapshot => {
-        callback(snapshot.val());
-    });
+  database.ref("Users/" + username).once("value").then(snapshot => {
+    callback(snapshot.val());
+  });
 }
 
 function updateTransactions(username, transaction) {
-    console.log("in updateTrans");
-    getTransactions(username, function(transactions) {
-        transactions.push(transaction);
-        database.ref("Users/"+username+"/transactions").set(transactions);
-    });
-
+  console.log("in updateTrans");
+  getTransactions(username, function (transactions) {
+    transactions.push(transaction);
+    database.ref("Users/" + username + "/transactions").set(transactions);
+  });
 }
 
 // call like this
@@ -32,7 +30,7 @@ function updateTransactions(username, transaction) {
 //     window.alert(transactions[0].amount);
 // });
 function getTransactions(username, callback) {
-    database.ref("Users/"+username+"/transactions").once("value").then(snapshot => {
-        callback(snapshot.val());
-    });
+  database.ref("Users/" + username + "/transactions").once("value").then(snapshot => {
+    callback(snapshot.val());
+  });
 }
--- scripts/transactions.js
+++ scripts/transactions.js
@@ -1,11 +1,14 @@
 $(function () {
+
+  getBtcUsd(function (val) {
+    $("#priceLimit").val(val);
+  });
+
   $(".place-order-form").on('submit', function (e) {
     e.preventDefault();
     updateTransactions(Cookies.get('username'), {
       amount: $("#quantity").val(),
-      buyPrice: getBtcUsd(function (val) {
-        return val;
-      })
-    })
+      buyPrice: $("#priceLimit").val()
+    });
   });
 });
