d51b98ffd5e0354c9b42aae906aebb3394ae27e8
Justin <jrosenthal96@gmail.com>
2017-12-05 22:08:26 -0500

    Attempt at add transaction

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,17 @@
 const rsskey = "b9f08983e0e54457927f26769123b954";
 
+function getBtcUsd(callback) {
+  $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", d => {
+    callback(d.bpi.USD.rate);
+  });
+}
+
 $(function () {
   if (Cookies.get('isLoggedIn') !== 'true') {
     window.location.href = "../index.html";
   }
 
-  $.getJSON("https://api.coindesk.com/v1/bpi/currentprice.json", d => {
-    $(".conversion .btcusd").html(d.bpi.USD.rate)
+  getBtcUsd(function (val) {
+    $(".conversion .btcusd").html(val);
   });
 });
--- scripts/transactions.js
+++ scripts/transactions.js
@@ -1,24 +1,11 @@
 $(function () {
   $(".place-order-form").on('submit', function (e) {
     e.preventDefault();
-
-    let stockstatus = $('#stockStatus');
-    if (Math.floor(Math.random() * (2 - 1 + 1) + 1) === 1) {
-      stockstatus.attr('src', '../images/stockdown.png');
-    } else {
-      stockstatus.attr('src', '../images/stockup.png');
-    }
-
-    var cashBalanceDollar = Math.floor(Math.random() * (8000 - 1500 + 1500) + 1500);
-    var cashBalanceCent = Math.floor(Math.random() * (99 - 10 + 10) + 10);
-    $('#cashBalance').html('$' + cashBalanceDollar + '.' + cashBalanceCent);
-
-    var todayNetChangeDollar = Math.floor(Math.random() * (500 - 80 + 80) + 80);
-    var todayNetChangeCent = Math.floor(Math.random() * (99 - 10 + 10) + 10);
-    $('#todayNetChange').html('$' + todayNetChangeDollar + '.' + todayNetChangeCent);
-
-    var accountValueDollar = Math.floor(Math.random() * (200000 - 60000 + 60000) + 60000);
-    var accountValueCent = Math.floor(Math.random() * (99 - 10 + 10) + 10);
-    $('#accountValue').html('$' + accountValueDollar + '.' + accountValueCent);
+    newTransaction({
+      amount: $("#quantity").val(),
+      buyPrice: getBtcUsd(function (val) {
+        return val;
+      })
+    })
   });
 });
